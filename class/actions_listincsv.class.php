c40f73a6b9bfcd4a7db4bbccb843d249e452a7be
Maxime Kohlhaas <maxime@atm-consulting.fr>
2017-07-22 10:44:34 +0200

    First commit

---
+++ class/actions_listincsv.class.php
@@ -0,0 +1,127 @@
+<?php
+/* <one line to give the program's name and a brief idea of what it does.>
+ * Copyright (C) 2015 ATM Consulting <support@atm-consulting.fr>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * \file	class/actions_listincsv.class.php
+ * \ingroup listincsv
+ * \brief   This file is an example hook overload class file
+ *		  Put some comments here
+ */
+
+/**
+ * Class ActionsListInCSV
+ */
+class ActionsListInCSV
+{
+	/**
+	 * @var array Hook results. Propagated to $hookmanager->resArray for later reuse
+	 */
+	public $results = array();
+
+	/**
+	 * @var string String displayed by executeHook() immediately after return
+	 */
+	public $resprints;
+
+	/**
+	 * @var array Errors
+	 */
+	public $errors = array();
+
+	/**
+	 * Constructor
+	 */
+	public function __construct()
+	{
+	}
+
+	/**
+	 * printCommonFooter
+	 *
+	 * @param   array()		 $parameters	 Hook metadatas (context, etc...)
+	 * @param   CommonObject	&$object		The object to process (an invoice if you are in invoice module, a propale in propale's module, etc...)
+	 * @param   string		  &$action		Current action (if set). Generally create or edit or null
+	 * @param   HookManager	 $hookmanager	Hook manager propagated to allow calling another hook
+	 * @return  int							 < 0 on error, 0 on success, 1 to replace standard code
+	 */
+	function printCommonFooter($parameters, &$object, &$action, $hookmanager)
+	{
+		if (strpos($parameters['context'], 'list') !== false)
+		{
+			$pathtojs = dol_buildpath('/listincsv/js/listincsv.js.php',1);
+			$pathtoimg = dol_buildpath('/listincsv/img/listincsv.png',1);
+			//$pathtoimg = dol_buildpath('/theme/eldy/img/upload.png',1);
+
+			$link = '<a href="#" class="export" style="text-decoration: none;">';
+			$endlink = '</a>';
+			$img = ' <img src="'.$pathtoimg.'" style="vertical-align: middle;" width="20" />';
+			
+			$download = $link . $img . $endlink;
+			
+			// Inclusion d'un JS qui va permettre de télécharger la liste
+			?>
+			<script type="text/javascript" language="javascript" src="<?php echo $pathtojs; ?>"></script>
+			<script type="text/javascript" language="javascript">
+			
+			$(document).ready(function() {
+				$('div.titre').append('<?php echo $download; ?>');
+				$(".export").on('click', function(event) {
+					// Récupération des données du formulaire de filtre et transformation en objet
+					var $form = $('form[name="formfilter"]');
+					var data = objectifyForm($form.serializeArray());
+					
+					// Pas de limite, on veut télécharger la liste totale
+					data.limit = 10000000;
+					
+					var $self = $(this);
+					
+					// Envoi de la requête HTTP en mode synchrone
+					$.ajax({
+						url: $form.attr('action'),
+						type: $form.attr('method'),
+						data: data,
+						async: false
+					}).done(function(html) {
+						// Récupération de la table html qui nous intéresse
+						var $table = $(html).find('table.liste');
+						
+						// Nettoyage de la table avant conversion en CSV
+						// Suppression de la 2ème ligne qui contient les filtres de la liste
+						$table.find('tr:eq(1)').remove();
+						// Suppression de la dernière colonne qui contient seulement les loupes des filtres
+						$table.find('th:last-child, td:last-child').remove();
+						// Suppression de la ligne TOTAL en pied de tableau
+						$table.find('tr.liste_total').remove();
+						// Remplacement des sous-table par leur valeur text(), notamment pour la ref dans les listes de propales, factures...
+						$table.find('td > table').map(function(i, cell) {
+							$cell = $(cell);
+							$cell.html($cell.text());
+						});
+						
+						// Transformation de la table liste en CSV + téléchargement
+						var args = [$table, 'export.csv'];
+						exportTableToCSV.apply($self, args);
+					});
+				});
+			});
+			
+			</script>
+			<?php
+		}
+	}
+}
---
+++ js/listincsv.js.php
@@ -0,0 +1,85 @@
+// Function found here : https://stackoverflow.com/questions/16078544/export-to-csv-using-jquery-and-html
+function exportTableToCSV($table, filename) {
+					
+	var $rows = $table.find('tr:has(th),tr:has(td)'),
+
+	// Temporary delimiter characters unlikely to be typed by keyboard
+	// This is to avoid accidentally splitting the actual contents
+	tmpColDelim = String.fromCharCode(11), // vertical tab character
+	tmpRowDelim = String.fromCharCode(0), // null character
+
+	// actual delimiter characters for CSV format
+	colDelim = '","',
+	rowDelim = '"\r\n"',
+
+	// Grab text from table into CSV formatted string
+	csv = '"' + $rows.map(function(i, row) {
+		var $row = $(row),
+		$cols = $row.find('th,td');
+
+		return $cols.map(function(j, col) {
+			var $col = $(col),
+			text = $col.text().trim();
+			//$col.css('background-color','red');
+			//alert(i+' - '+j+' : '+text);
+			// Spécifique pour "nettoyer" les données
+			// Si texte vide, on cherche une image et on prend le title
+			if(text == '' && $col.find('img').length > 0) {
+				text = $col.find('img').attr('title').trim();
+			}
+
+			return text.replace(/"/g, '""'); // escape double quotes
+
+		}).get().join(tmpColDelim);
+
+	}).get().join(tmpRowDelim)
+	.split(tmpRowDelim).join(rowDelim)
+	.split(tmpColDelim).join(colDelim) + '"';
+
+	// Deliberate 'false', see comment below
+	if (false && window.navigator.msSaveBlob) {
+
+		var blob = new Blob([decodeURIComponent(csv)], {
+			type: 'text/csv;charset=utf8'
+		});
+
+		// Crashes in IE 10, IE 11 and Microsoft Edge
+		// See MS Edge Issue #10396033
+		// Hence, the deliberate 'false'
+		// This is here just for completeness
+		// Remove the 'false' at your own risk
+		window.navigator.msSaveBlob(blob, filename);
+
+	} else if (window.Blob && window.URL) {
+		// HTML5 Blob
+		var blob = new Blob([csv], {
+			type: 'text/csv;charset=utf-8'
+		});
+		var csvUrl = URL.createObjectURL(blob);
+
+		$(this)
+		.attr({
+			'download': filename,
+			'href': csvUrl
+		});
+	} else {
+		// Data URI
+		var csvData = 'data:application/csv;charset=utf-8,' + encodeURIComponent(csv);
+
+		$(this)
+		.attr({
+			'download': filename,
+			'href': csvData,
+			'target': '_blank'
+		});
+	}
+}
+
+//serialize data function
+function objectifyForm(formArray) { 
+	var returnArray = {};
+	for (var i = 0; i < formArray.length; i++) {
+		returnArray[formArray[i]['name']] = formArray[i]['value'];
+	}
+	return returnArray;
+}
