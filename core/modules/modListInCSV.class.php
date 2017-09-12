b7c80c41dbffff543b2439dcc71b2e1c50e49633
Maxime Kohlhaas <maxime@atm-consulting.fr>
2017-09-12 11:03:17 +0200

    Module is compatible from 3.9

--- core/modules/modListInCSV.class.php
+++ core/modules/modListInCSV.class.php
@@ -103,7 +103,7 @@ class modListInCSV extends DolibarrModules
 		$this->requiredby = array();	// List of modules id to disable if this one is disabled
 		$this->conflictwith = array();	// List of modules id this module is in conflict with
 		$this->phpmin = array(5,0);					// Minimum version of PHP required by module
-		$this->need_dolibarr_version = array(5,0);	// Minimum version of Dolibarr required by module
+		$this->need_dolibarr_version = array(3,9);	// Minimum version of Dolibarr required by module
 		$this->langfiles = array("listincsv@listincsv");
 
 		// Constants
