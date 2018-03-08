16a8b4c4ee5c84a2bc19d923e97127dc6eb46e8d
David <david@code42.fr>
2018-03-08 14:43:52 +0100

    - Ajout du caractère DOM en debut de CSV pour l'export UTF-8 - Remplacement du sépatateur par un point-virgule.

--- js/listincsv.js.php
+++ js/listincsv.js.php
@@ -9,11 +9,11 @@ function exportTableToCSV($table, filename) {
 	tmpRowDelim = String.fromCharCode(0), // null character
 
 	// actual delimiter characters for CSV format
-	colDelim = '","',
+	colDelim = '";"', // CODE42 : Change coma to semicolon to match Excel default CSV format.
 	rowDelim = '"\r\n"',
 
 	// Grab text from table into CSV formatted string
-	csv = '"' + $rows.map(function(i, row) {
+	csv = '\ufeff"' + $rows.map(function(i, row) { // CODE42 : Adding ‘\ufeff’ (BOM) at the start of csv to correctly export UTF-8 Chars.
 		var $row = $(row),
 		$cols = $row.find('th,td');
 
