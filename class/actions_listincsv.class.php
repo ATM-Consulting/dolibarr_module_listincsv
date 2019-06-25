7793f588450dd6d9e2f37e7a27b0e72fdf80bc58
atm-greg <gregory.blemand@atm-consulting.fr>
2019-06-25 16:51:33 +0200

    fix array values in data

--- class/actions_listincsv.class.php
+++ class/actions_listincsv.class.php
@@ -107,9 +107,9 @@ class ActionsListInCSV
 						// Pas de limite, on veut télécharger la liste totale
 						data.limit = 10000000;
 						data.socid = <?php echo $socid; ?>;
-						
+
 						var $self = $(this);
-						
+
 						$('#dialogforpopup').html('<?php echo ($langs->trans('FileGenerationInProgress')); ?>');
 						$('#dialogforpopup').dialog({
 							open : function(event, ui) {
@@ -122,20 +122,20 @@ class ActionsListInCSV
 								}).done(function(html) {
 									// Récupération de la table html qui nous intéresse
 									var $table = $(html).find('table.liste');
-									
+
 									// Nettoyage de la table avant conversion en CSV
-									
+
 									// Suppression des filtres de la liste
 									$table.find('tr.liste_titre_filter').remove(); // >= 6.0
 									$table.find('tr:has(td.liste_titre)').remove(); // < 6.0
-									
+
 									// Suppression de la dernière colonne qui contient seulement les loupes des filtres
                                     $table.find('th:last-child, td:last-child').each(function(index){
                                         $(this).find('dl').remove();
                                        if($(this).closest('table').hasClass('liste')) $(this).remove();
                                     });
 
-									
+
 									// Suppression de la ligne TOTAL en pied de tableau
                                     <?php if(empty($conf->global->LISTINCSV_DONT_REMOVE_TOTAL)) { ?> $table.find('tr.liste_total').remove(); <?php } ?>
 
@@ -144,11 +144,11 @@ class ActionsListInCSV
 										$cell = $(cell);
 										$cell.html($cell.text());
 									});
-									
+
 									// Transformation de la table liste en CSV + téléchargement
 									var args = [$table, 'export.csv'];
 									exportTableToCSV.apply($self, args);
-									
+
 									$('#dialogforpopup').dialog('close');
 								});
 							}
--- js/listincsv.js.php
+++ js/listincsv.js.php
@@ -88,7 +88,12 @@ function exportTableToCSV($table, filename) {
 function objectifyForm(formArray) {
 	var returnArray = {};
 	for (var i = 0; i < formArray.length; i++) {
-		returnArray[formArray[i]['name']] = formArray[i]['value'];
+		if (formArray[i]['name'].substring(formArray[i]['name'].length - 2) == "[]") {
+			var name = formArray[i]['name'].substring(0, formArray[i]['name'].length - 2);
+			if (!returnArray.hasOwnProperty(name)) returnArray[name] = [];
+			returnArray[name].push(formArray[i]['value']);
+		}
+		else returnArray[formArray[i]['name']] = formArray[i]['value'];
 	}
 	return returnArray;
 }
